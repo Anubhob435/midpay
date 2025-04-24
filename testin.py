569feffc7fe5601379288da3f305186f84db940b
Anubhob <anubhob493@gmail.com>
2025-04-24 23:22:40 +0530

    client testing

--- testin.py
+++ testin.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "http://localhost:5000/api/accounts"
+url = "http://192.168.0.121:5000/api/accounts"
 headers = {
-    "X-API-Key": "avhgavhgvd"
+    "X-API-Key": "2adce4844b0133b3047657e76c4bda9d0143c9752e8c6bf5599dd3fd80319167"
 }
 
 response = requests.get(url, headers=headers)
